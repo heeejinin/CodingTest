cd0272cac8efd6f6ab00f2361d43732e040c297a
heeejinin <150228975+heeejinin@users.noreply.github.com>
2024-11-08 02:47:51 +0900

    [99클럽 - 비기너] Day 11
    11. 프로그래머스 [완주하지 못한 선수]

---
+++ CodingTest/Club99/beginner/Main11.java
@@ -0,0 +1,21 @@
+package Club99.beginner;
+
+import java.util.Arrays;
+
+public class Main11 {
+    public String solution(String[] participant, String[] completion) {
+        // 1. 참가자와 완주자 배열을 정렬
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        // 2. 정렬된 배열을 순차적으로 비교하여 다른 이름을 찾기
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        // 3. 만약 모두 일치하면 마지막 참가자가 완주하지 못한 사람임
+        return participant[participant.length - 1];
+    }
+}
