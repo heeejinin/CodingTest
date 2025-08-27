bdc732e673e6365c0a8af0c283aac8f1ebbed322
강희진 <150228975+heeejinin@users.noreply.github.com>
2025-08-27 18:10:26 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.90 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=java) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 11.80 ms
+메모리: 75.2 MB, 시간: 11.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 27일 18:08:55
+2025년 08월 27일 18:10:36
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -5,11 +5,7 @@ class Solution {
         String result1 = a + "" + b;
         String result2 = b + "" + a;
         
-        if(Integer.parseInt(result1) > Integer.parseInt(result2)){
-            answer = Integer.parseInt(result1);
-        } else{
-            answer = Integer.parseInt(result2);
-        }
+        answer = Integer.parseInt(result1) > Integer.parseInt(result2) ? Integer.parseInt(result1) : Integer.parseInt(result2);
         
         return answer;
     }
