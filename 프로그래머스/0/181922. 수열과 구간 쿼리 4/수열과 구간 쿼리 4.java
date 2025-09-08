4ecf47f93b4b23be88c6f783ffdbeabd7a6ba4f9
강희진 <150228975+heeejinin@users.noreply.github.com>
2025-09-09 00:10:57 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.56 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (i % k == 0) { // i가 k의 배수인지 확인
+                    arr[i] += 1;
+                }
+            }
+        }
+        return arr;
+    }
+}
