e0ac427a7846e9ce2c28714974628618bf37039a
강희진 <150228975+heeejinin@users.noreply.github.com>
2025-09-28 21:11:26 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        int combination = 1;
+        
+        for(int count : map.values()) {
+            combination *= (count + 1);
+        }
+        
+        return combination - 1;
+    }
+}
